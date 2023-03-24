f12b04bf93a13949e5d242150865c0699a5a6e54
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2023-03-24 15:42:47 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -93,8 +93,8 @@
         <div class="main-menu d-none d-lg-block col-lg-2 col-xl-2 navbar-container"><? include("includes/menu.php"); ?></div>
 
         <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 main-content">
-          <div class="content container-fluid m-0">
-            <div class="row">
+          <div class="content container-fluid m-0" id="user-page-content">
+            <div class="row" id="user-page-content-row">
 <?
               include("includes/user_page/user-info.php");
               include("includes/user_page/user-content.php");
