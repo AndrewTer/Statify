3221d9199ee71207083e8a868eec3f5af9d1e001
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2023-04-04 12:38:29 +0300

    Add files via upload

--- modals/photo-modal/btn-rating.php
+++ modals/photo-modal/btn-rating.php
@@ -5,7 +5,7 @@
 	if ($ban_check == 'success')
 		if ($user_rating_value >= 1 && $user_rating_value <= 5)
 		{
-			echo '<div class="m-0 p-1 text-center d-flex flex-row justify-content-center photo-menu-center">';
+			echo '<div class="m-0 p-1 text-center d-flex flex-row justify-content-center align-items-center photo-menu-center">';
 			for ($stars_count = 0; $stars_count < 5; $stars_count++)
 				if ($stars_count <= $user_rating_value - 1)
 					echo '<i class="fa fa-star-o active-star fz-18 p-1" aria-hidden="true"></i>';
@@ -16,11 +16,15 @@
 			if (check_email_confirmed($current_user_uuid))
 				if (get_latest_avatar($current_user_uuid))
 					echo '<div class="m-0 p-1 text-center d-flex flex-row justify-content-center align-items-center current-photo-menu photo-menu-center" id="rating-result">
-									<div class="nav-item w-100 m-0 p-0 d-flex flex-row justify-content-center align-items-center">
-										<a class="nav-link w-100 m-0 p-0 text-center" onclick="displayBlockSetRating();">
-											<i class="fa fa-star-half-o fz-18 p-1 m-0" aria-hidden="true"></i>
-										</a>
-									</div>
+									<a class="nav-link w-100 m-0 p-0 text-center" onclick="displayBlockSetRating();">
+										<div class="nav-item w-100 m-0 p-0 d-flex flex-row justify-content-center align-items-center">
+											<p class="m-0 p-0">
+												<svg class="m-0 p-0" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+													<path fill-rule="evenodd" clip-rule="evenodd" d="M13.1467 4.13112C12.7115 3.12752 11.2883 3.12751 10.8531 4.13112L8.87333 8.69665L3.91947 9.16869C2.83051 9.27246 2.3907 10.626 3.2107 11.3501L6.94099 14.6438L5.85911 19.501C5.62129 20.5688 6.77271 21.4053 7.7147 20.8492L11.9999 18.3193L16.2851 20.8492C17.2271 21.4053 18.3785 20.5688 18.1407 19.501L17.0588 14.6438L20.7891 11.3501C21.6091 10.626 21.1693 9.27246 20.0804 9.16869L15.1265 8.69665L13.1467 4.13112ZM12 15.9968L12.5083 16.2969L15.8125 18.2477L14.9783 14.5023L14.85 13.9261L15.2925 13.5353L18.1689 10.9956L14.3491 10.6316L13.7613 10.5756L13.5265 10.034L12 6.51388V15.9968Z" fill="var(--main-text-color)"></path>
+												</svg>
+											</p>
+										</div>
+									</a>
 								</div>';
 				else
 					echo '<div class="m-0 p-1 text-center d-flex flex-row justify-content-center align-items-center photo-menu-center">
--- modals/photo-modal/btn-save.php
+++ modals/photo-modal/btn-save.php
@@ -5,13 +5,21 @@
 		echo '<a class="nav-link p-0 text-center d-flex flex-row justify-content-center align-items-center" 
 							style="border: none;" 
 							onclick="event.preventDefault();unsavePicture(\''.$current_user_uuid.'\',\''.$user_uuid.'\',\''.$picture_name.'\');">
-						<i class="fa fa-bookmark fz-18 p-1 m-0 save-icon" aria-hidden="true"></i>
+						<p class="m-0 p-0">
+							<svg width="22px" height="22px" viewBox="0 0 24 24" fill="none">
+								<path d="M5.5 1C4.39543 1 3.5 1.89543 3.5 3V22C3.5 22.3612 3.6948 22.6944 4.00961 22.8715C4.32441 23.0486 4.71028 23.0422 5.01903 22.8548L12 18.6157L18.981 22.8548C19.2897 23.0422 19.6756 23.0486 19.9904 22.8715C20.3052 22.6944 20.5 22.3612 20.5 22V3C20.5 1.89543 19.6046 1 18.5 1H5.5Z" fill="var(--save-icon-color)"></path>
+							</svg>
+						</p>
 					</a>';
 	else
 		echo '<a class="nav-link p-0 text-center d-flex flex-row justify-content-center align-items-center" 
 							style="border: none;" 
 							onclick="event.preventDefault();savePicture(\''.$current_user_uuid.'\',\''.$user_uuid.'\',\''.$picture_name.'\');">
-						<i class="fa fa-bookmark-o fz-18 p-1 m-0" aria-hidden="true"></i>
+						<p class="m-0 p-0">
+							<svg width="22px" height="22px" viewBox="0 0 24 24" fill="none">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 3C3.5 1.89543 4.39543 1 5.5 1H18.5C19.6046 1 20.5 1.89543 20.5 3V22C20.5 22.3612 20.3052 22.6944 19.9904 22.8715C19.6756 23.0486 19.2897 23.0422 18.981 22.8548L12 18.6157L5.01903 22.8548C4.71028 23.0422 4.32441 23.0486 4.00961 22.8715C3.6948 22.6944 3.5 22.3612 3.5 22V3ZM18.5 3L5.5 3V20.2228L11.481 16.591C11.7999 16.3974 12.2001 16.3974 12.519 16.591L18.5 20.2228V3Z" fill="var(--main-text-color)"></path>
+							</svg>
+						</p>
 					</a>';
 ?>
 	</div>
